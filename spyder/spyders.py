0d6e4f0e18b1e87ed459a70aad43638e176943d5
CrabGeek <xgp980303@gmail.com>
2024-05-24 00:05:04 +0800

    update

--- spyder/spyders.py
+++ spyder/spyders.py
@@ -3,27 +3,6 @@ import scrapy
 from scrapy.crawler import CrawlerProcess
 from scrapy.http import Response
 
-# class QuotesSpider(scrapy.Spider):
-#     name = "quotes"
-
-#     def start_requests(self):
-#         yield scrapy.Request(url='https://www.baidu.com', callback=self.parse)
-
-#     def parse(self, response):
-#         page = response.url.split("/")[-2]
-#         filename = f'quotes-{page}.html'
-#         with open(filename, 'wb') as f:
-#             f.write(response.body)
-#         self.log(f'Saved file {filename}')
-
-
-# if __name__ == '__main__':
-#     process = CrawlerProcess()
-
-#     process.crawl(QuotesSpider)
-#     process.start() # the script will block here until the crawling is finished\
-
-
 class WeBoSpider(scrapy.Spider):
     name = "WeboSpider"
 
@@ -43,3 +22,22 @@ class RedNotesSpider(scrapy.Spider):
 
     def parse(self, response) :
         return
+    
+
+class TouTiaoSpider(scrapy.Spider):
+    name = "TouTiaoSpider"
+
+    start_urls = [
+        "https://www.toutiao.com"
+    ]
+    
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        filename = 'quotes.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+
+
+if __name__ == '__main__':
+    process = CrawlerProcess()
+    process.crawl(TouTiaoSpider)
+    process.start()
