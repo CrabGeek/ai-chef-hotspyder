612d2c8361c17f85fac70404cbd39ce834c098f0
CrabGeek <xgp980303@gmail.com>
2024-06-06 06:06:25 +0800

    update

--- chef_mq/client.py
+++ chef_mq/client.py
@@ -2,40 +2,72 @@ import pika
 from chef_utility.hotspyder_utility import MQ_CHANNEL_CONSUMMING_POOL
 
 
-MQ_CHANNEL_MAP = {}
+
+class MQClient():
+    
+    def __init__(self, address: str) -> None:
+        self.__addr = address
+        self.__publish_conn: pika.BlockingConnection = None
+        self.__consume_conn: pika.BlockingConnection = None
+        
+    def __init_connections(self):
+        try:
+            self.__publish_conn = pika.BlockingConnection(
+                pika.ConnectionParameters(host=self.__addr)
+            )
+            self.__consume_conn = pika.BlockingConnection(
+                pika.ConnectionParameters(host=self.__addr)
+            )
+
+        except Exception as e:
+            print(e)
+
+
+__publish_conn: pika.BlockingConnection = None
+__consume_conn: pika.BlockingConnection = None
 
 
 def __init_MQ():
+    global __publish_conn
+    global __consume_conn
+
     try:
-        conn = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
-        channel = conn.channel()
-        channel.queue_declare("hotspyder_to_doorman")
-        MQ_CHANNEL_MAP.update({"hotspyder_to_doorman": channel})
+        __publish_conn = pika.BlockingConnection(
+            pika.ConnectionParameters(host="localhost")
+        )
+        __consume_conn = pika.BlockingConnection(
+            pika.ConnectionParameters(host="localhost")
+        )
+
     except Exception as e:
         print(e)
 
 
-def send(channel_name: str, payload: str, properties: pika.BasicProperties):
-    channel = MQ_CHANNEL_MAP.get(channel_name)
-    if channel is None:
-        # TODO: need to log
-        return
-
-    try:
-        channel.basic_publish(
-        exchange="", routing_key=channel_name, body=payload, properties=properties
-    )
-    except Exception as e:
-        #TODO: need log
-        print(e)
+def send(
+    channel_name: str,
+    payload: str,
+    properties: pika.BasicProperties,
+    exchange: str = "",
+):
+    with __publish_conn.channel() as channel:
+        try:
+            channel.queue_declare(channel_name)
+            channel.basic_publish(
+                exchange=exchange,
+                routing_key=channel_name,
+                body=payload,
+                properties=properties,
+            )
+        except Exception as e:
+            # TODO: need to log
+            print(e)
 
 
 def start_consuming(channel_name: str):
-    channel = MQ_CHANNEL_MAP.get(channel_name)
-    if channel is None:
-        # TODO: need to log
-        return
-    MQ_CHANNEL_CONSUMMING_POOL.submit(channel.start_consuming)
+    global __consume_conn
+
+    with __consume_conn.channel() as channel:
+        MQ_CHANNEL_CONSUMMING_POOL.submit(channel.start_consuming)
 
 
 __init_MQ()
--- chef_spyder/toutiao/pipelines.py
+++ chef_spyder/toutiao/pipelines.py
@@ -10,12 +10,17 @@ class TouTiaoPipeline:
 
     def process_item(self, item, spider):
         data = json.dumps(ItemAdapter(item).asdict(), ensure_ascii=False) + "\n"
-        hotspyder_utility.THREAD_POOL.submit(
-            lambda ctx: mq_client.send(*ctx),
-            (
-                "hotspyder_to_doorman",
-                data,
-                pika.BasicProperties(delivery_mode=DeliveryMode.Transient),
-            ),
-        )
+        print('-------------------------------')
+        print(data)
+        print('-------------------------------')
+
+        mq_client.send(channel_name="hotspyder_to_doorman", payload=data, properties=pika.BasicProperties(delivery_mode=DeliveryMode.Transient))
+        # hotspyder_utility.THREAD_POOL.submit(
+        #     lambda ctx: mq_client.send(*ctx),
+        #     (
+        #         "hotspyder_to_doorman",
+        #         data,
+        #         pika.BasicProperties(delivery_mode=DeliveryMode.Transient),
+        #     ),
+        # )
         return item
